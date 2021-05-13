2a33e408ca8617f2ef3eca58d62b40b4460fac41
karthikgcan <karthikganeshan.rmd@gmail.com>
2021-05-13 21:49:17 +0530

    c

---
+++ routes/posts.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import  {getPosts , createPost} from '../controllers/posts.js'
+import PostMessage from '../models/postMessage.js'
+
+
+const router = express.Router();
+
+//localhost:5000/posts
+router.get('/', async (req,res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({message:error.message});
+    }} );
+router.post('/', async (req,res) => {
+    const cbody = req.body;
+    console.log(cbody);
+    const newpost = new PostMessage(cbody);
+    try {
+        await newpost.save();
+        res.status(201).json(newpost);
+    } catch (error) {
+        res.status(409).json({message:error.message});
+    }
+} );
+
+export default router;
